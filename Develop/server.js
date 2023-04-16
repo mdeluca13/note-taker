6288005191ea2cdbf94cabe9d350d4a388d6214f
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-04-16 14:17:32 -0400

    started delete function

--- Develop/server.js
+++ Develop/server.js
@@ -27,19 +27,31 @@ app.get('/notes', (req, res) =>
 
 // Setting API Routes
 app.get('/api/notes', (req, res) => {
-  // console.log(`api get notes data ${JSON.stringify(noteData)}`);
   res.json(noteData)
-
 }
 );
 
 app.post('/api/notes', (req, res) => {
-  // console.log(`req.body ${JSON.stringify(req.body)}`);
   noteData.push(req.body)
-  // console.log(`what is noteData ${JSON.stringify(noteData)}`)
   fs.writeFile('./db/db.json', JSON.stringify(noteData), (err) => 
   err ? console.log(err) : console.log('Working'))
 });
+
+// app.get('/api/notes/:notes', (req, res) => {
+//   console.log(req.params)
+//   // Coerce the specific search term to lowercase
+//   const requestedNote = req.params.notes.toLowerCase();
+
+//   // Iterate through the note name name to check if it matches `req.params.notes`
+//   for (let i = 0; i < noteData.length; i++) {
+//     if (requestedNote === noteData[i].notes.toLowerCase()) {
+//       return res.json(noteData[i]);
+//     }
+//   }
+
+//   // Return a message if the term doesn't exist in our DB
+//   return res.json('No match found');
+// });
 
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
